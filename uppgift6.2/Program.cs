3149584fbbf7fea3de4d28715108663782519a07
en301 <emilsjostrand1000@gmail.com>
2024-02-14 10:38:19 +0100

    Add project files.

---
+++ uppgift6.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uppgift_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 2 tal");
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Största talet var "+ Störst(tal1, tal2));
+            Console.ReadKey();
+        }
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 >= tal2)
+            {
+
+                return tal1;
+            }
+            else
+            {
+                return tal2;
+            }
+        }
+    }
+}
